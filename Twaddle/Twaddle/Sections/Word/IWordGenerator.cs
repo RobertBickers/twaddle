96a790ca29cf97301c860a595d56ffb51a4b0ea6
Robert Bickers <robert-bickers@live.co.uk>
2018-05-02 20:14:13 +0100

    Update to documentation and inclusion within future builds.

--- Twaddle/Twaddle/Sections/Word/IWordGenerator.cs
+++ Twaddle/Twaddle/Sections/Word/IWordGenerator.cs
@@ -16,9 +16,9 @@ namespace Bickers.Twaddle.Contracts
         /// Generates a random sentence with the provided number of words
         /// </summary>
         /// <param name="numberOfWords">The number of words that will be included in the sentence</param>
-        /// <param name="startingWord">(Optional) The word that the sentencee will begin with</param>
-        /// <param name="endingWord">(Optional) The word that the sentence will end with</param>
+        /// <param name="append">(Optional) The word that the sentencee will begin with</param>
+        /// <param name="prepend">(Optional) The word that the sentence will end with</param>
         /// <returns></returns>
-        string GenerateSentence(int numberOfWords, string startingWord = "", string endingWord = "");
+        string GenerateSentence(int numberOfWords, string append = "", string prepend = "");
     }
 }
